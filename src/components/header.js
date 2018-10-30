e302fa756c7d431a518f9f3a53c3adaaa75ecc7b
Admin <admin@MacBook-Pro-Admin.local>
2018-10-30 04:05:20 +0200

    Page upd

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="logo">
+      <Link class="logo" to="/">eRoad</Link>
+    </div>
+    <div className="menu-items">
+      <Link class="logo" to="/">Works</Link>
+      <Link class="logo" to="/">Partners</Link>
+      <Link class="logo" to="/">Team</Link>
+      <Link class="logo" to="/">Feedback</Link>
+      <Link class="logo" to="/">Contacts</Link>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,18 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Personal sales team for your business.</h1>
-    <p>Customer friendly support innovative positive precise timely.</p>
-    <p>E-Road is a financial partner of Radio Button software development company.</p>
-    <p>Being based in Krakow E-Road develops and maintains high-loaded projects.</p>
-    <Link to="/page-2/">Scroll Down</Link>
+        <div className="Hero">
+          <div className="LeftTop">
+            <h1 class="top">Personal sales team</h1>
+            <h1 class="top-red">for your business.</h1>
+            <p class="title-description">Customer friendly support innovative positive precise timely.</p>
+            <p>E-Road is a financial partner of <a href="http://radiobutton.io/">Radio Button</a> software development company.</p>
+            <p>Being based in Krakow E-Road develops and maintains high-loaded projects.</p>
+            <p class='tip'>Scroll Down</p>
+          </div>
+          <div className="RightTop">
+          </div>
+        </div>
   </Layout>
 )
 
